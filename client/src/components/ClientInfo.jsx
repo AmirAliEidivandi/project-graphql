9594ddd7c0844263050b463067935f898b2b3235
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-21 22:41:56 +0430

    hasn't bug fix

--- client/src/components/ClientInfo.jsx
+++ client/src/components/ClientInfo.jsx
@@ -1,18 +1,19 @@
 import { FaEnvelope, FaPhone, FaIdBadge } from "react-icons/fa";
 
 export default function ClientInfo({ client }) {
+    console.log(client)
     return (
         <>
             <h5 className="mt-5">Client Information</h5>
             <ul className="list-group">
                 <li className="list-group-item">
-                    <FaIdBadge className="icon" /> {client.name}
+                    <FaIdBadge className="icon" />
                 </li>
                 <li className="list-group-item">
-                    <FaEnvelope className="icon" /> {client.email}
+                    <FaEnvelope className="icon" />
                 </li>
                 <li className="list-group-item">
-                    <FaPhone className="icon" /> {client.phone}
+                    <FaPhone className="icon" />
                 </li>
             </ul>
         </>
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -10,12 +10,13 @@ export default function Project() {
 
     if (loading) return <Spinner />;
     if (error) return <p>Something Went Wrong</p>;
+    console.log(data)
 
     return (
         <>
             {!loading && !error && (
                 <div className="mx-auto w-75 card p-5">
-                    <Link to={"/"} className="btn btn-light btn-sm w-2 d-inline ms-auto">
+                    <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
                         Back
                     </Link>
 
